aa2183c099f9882a6090c3214bfe0f8052fb4b8b
Danny Chan <dchan@coronalabs.com>
2014-10-16 14:22:15 -0700

    Add pasteboard to enterprise

--- Corona/config.lua
+++ Corona/config.lua
@@ -1,14 +1,15 @@
 application = 
 {
-	content = 
-	{ 
-		width = 320,
-		height = 480,
-		scale = "letterbox",
-		fps = 60,
-		
-		imageSuffix = {
-			["@2x"] = 2,
-		}
-	}
+    content = 
+    {   
+        width = 320,
+        height = 480,
+        scale = "letterbox",
+        fps = 60,
+        xAlign = "center",
+        yAlign = "center",
+        imageSuffix = {
+            ["@2x"] = 2,
+        }
+    }
 }
